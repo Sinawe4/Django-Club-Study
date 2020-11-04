efe30fefa51dfc3971ad17fbab2feba78007a3e9
Ubuntu <ubuntu@ip-172-31-93-15.ec2.internal>
2020-11-04 09:31:32 +0000

    First Django Club Study

---
+++ gsm/posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from .models import Post
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+   posts = Post.objects.all()
+   context = {'posts' : posts}
+   
+   return render (request, 'posts/index.html', context)
+   
+def detail(request, post_id):
+   post = Post.objects.get(id = post_id)
+   context = {'post' : post}
+   
+   return render(request, 'posts/detail.html', context)
